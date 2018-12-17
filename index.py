5f0c43bf3a09cd6891ceea0e0cb8b490cb840f61
Adithya <adithya.alankara@gmail.com>
2018-12-17 17:49:16 -0500

    asd

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -17,17 +19,13 @@ def reclist():
 	name = request.form['name']
 	return name
 
+@app.route("/predict")
+def get():
+	ext = 500
+	url = 'http://35.170.52.162/predict/'+repr(ext)
+	res = requests.get(url)
+	data = res.json()
+	return "The rating of the movie "+repr(data['product'])+" is "+repr(data['rating'])
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-# @app.route("/asd")
-# def get():
-#     try:
-#         res = requests.get(http://3.80.144.140/recommendations/1)
-#         return res.json()
-#     except:
-#         return False
-
-#@app.route("/asd")
-#data = get('http://3.80.144.140/recommendations/1')
-#print(data)
